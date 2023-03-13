284472c148b978d066321d2a7b2759a7df9254a9
unusualpseudo <13041487+unusualpseudo@users.noreply.github.com>
2023-03-14 00:54:02 +0100

    Run terraform only on changes modules

--- infrastructure/terraform/github/branch_protection.tf
+++ infrastructure/terraform/github/branch_protection.tf
@@ -10,7 +10,7 @@ resource "github_branch_protection" "main" {
 
 
   required_pull_request_reviews {
-    dismiss_stale_reviews = true
+    dismiss_stale_reviews      = true
     require_code_owner_reviews = true
     require_last_push_approval = true
   }
