7f28d92bf830f7243cefb560d7f1e0bf665c38d8
unusualpseudo <13041487+unusualpseudo@users.noreply.github.com>
2023-03-13 23:46:45 +0100

    Add github actions workflow for terrafom with connection to tf cloud

--- infrastructure/terraform/github/secrets.tf
+++ infrastructure/terraform/github/secrets.tf
@@ -10,3 +10,10 @@ resource "github_actions_secret" "app_id_private_key" {
   secret_name     = "BOT_APP_PRIVATE_KEY"
   plaintext_value = data.sops_file.github.data["app_private_key"]
 }
+
+
+resource "github_actions_secret" "tf_api_token" {
+  repository      = github_repository.repo.name
+  secret_name     = "TF_API_TOKEN"
+  plaintext_value = data.sops_file.github.data["tf_api_token"]
+}
