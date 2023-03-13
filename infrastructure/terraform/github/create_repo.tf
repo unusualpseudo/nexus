c0dfb2ead9481a87a7eb12df82318692addfadb2
unusualpseudo <13041487+unusualpseudo@users.noreply.github.com>
2023-03-13 23:03:59 +0100

    Update terrafor github module: update merge policy

--- infrastructure/terraform/github/create_repo.tf
+++ infrastructure/terraform/github/create_repo.tf
@@ -7,6 +7,12 @@ resource "github_repository" "repo" {
   license_template   = var.license_template
   topics             = var.topics
   auto_init          = var.auto_init
+  has_issues         = var.has_issues
+  has_discussions    = var.has_discussions
+  allow_auto_merge = var.allow_auto_merge
+  allow_merge_commit = var.allow_merge_commit
+  allow_squash_merge = var.allow_squash_merge
+  allow_rebase_merge = var.allow_rebase_merge
 }
 
 
@@ -15,14 +21,5 @@ resource "github_branch_default" "main" {
   branch     = "main"
   depends_on = [
     github_repository.repo
-  ]
-}
-
-resource "github_branch" "dev" {
-  branch        = "dev"
-  source_branch = github_branch_default.main.branch
-  repository    = github_repository.repo.name
-  depends_on = [
-    github_branch_default.main
   ]
 }
