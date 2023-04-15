c6b906373e81d5cf2cca68f34b0c5de2197ef532
unusualpseudo <13041487+unusualpseudo@users.noreply.github.com>
2023-04-16 00:37:47 +0200

    Add externaldns for cloudflare tunnel

--- infrastructure/terraform/cloudflare/tunnel.tf
+++ infrastructure/terraform/cloudflare/tunnel.tf
@@ -7,12 +7,3 @@ resource "cloudflare_tunnel" "homelab" {
   account_id = data.sops_file.cloudflare_secrets.data["cloudflare_account_id"]
   secret     = random_id.tunnel-secret.b64_std
 }
-
-
-resource "cloudflare_record" "wildcard" {
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  name    = "*"
-  value   = cloudflare_tunnel.homelab.cname
-  type    = "CNAME"
-  proxied = true
-}
